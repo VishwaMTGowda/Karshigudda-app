cc3ea2c1143ed24b0e999e01a529aab6eb699ccb
vishw <vishwamtgowda@gmail.com>
2026-09-11 21:45:44 +0530

    google ads added

--- app/layout.tsx
+++ app/layout.tsx
@@ -83,6 +83,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2040903055590766"
+     crossOrigin="anonymous"></script>
   <link
     rel="icon"
     type="image/png"
